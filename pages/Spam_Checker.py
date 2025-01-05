e58360388c89d61ebe94ef26ac749f3f1cdd55dc
Pushpendra Dhamanya <162273949+pushpendra0197@users.noreply.github.com>
2025-01-05 18:42:32 +0530

    Update Spam_Checker.py

--- pages/Spam_Checker.py
+++ pages/Spam_Checker.py
@@ -13,7 +13,7 @@ from sklearn.feature_extraction.text import CountVectorizer
 import joblib
 import streamlit as st
 cv=CountVectorizer()
-data=joblib.load(r"pages\clean_data_forfit")
+data=joblib.load(r"pages/clean_data_forfit")
 cv.fit(data)
 st.title("Spam Detection{messages and mails}")
 input_text=st.text_input("Enter your Mail subject Or Message")
@@ -28,7 +28,7 @@ if button:
        return(review)
     Input=clean(input_text)
     final_input=cv.transform([Input])
-    Model=joblib.load(r"pages\spam_mail&messages")
+    Model=joblib.load(r"pages/spam_mail&messages")
     pred=Model.predict(final_input)
     if pred==0:
         st.header("Not a spam")
